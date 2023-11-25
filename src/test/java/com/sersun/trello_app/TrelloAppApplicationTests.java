17a8866ea757770c6f1455cc0330ff0a4d0c1fa1
Denis Sersun <d.sersun@gmail.com>
2023-11-25 16:39:46 +0200

    Initial commit

---
+++ src/test/java/com/sersun/trello_app/TrelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sersun.trello_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
