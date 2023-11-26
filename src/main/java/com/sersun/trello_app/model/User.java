60bf9cb9443eafacb83e266c0e6d76203803074c
Denis Sersun <d.sersun@gmail.com>
2023-11-26 22:31:26 +0200

    Model, Repository, Service and controllers for User endpoints.

---
+++ src/main/java/com/sersun/trello_app/model/User.java
@@ -0,0 +1,46 @@
+package com.sersun.trello_app.model;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.UniqueElements;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Table(name = "users")
+public class User {
+
+    @Id
+    @Column(name = "userid")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer userId;
+
+    @NotEmpty
+    @Column(name = "username")
+    private String username;
+
+    @NotEmpty
+    @Column(name = "password")
+    private String password;
+
+    @NotEmpty
+    @Column(name = "email")
+    private String email;
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "userId=" + userId +
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
